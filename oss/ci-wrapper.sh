44447047122411f2f70f6e61c0ba22af6f25936e
Brian Gann <bkgann@gmail.com>
2019-02-18 00:03:34 -0600

    update circleci

--- oss/ci-wrapper.sh
+++ oss/ci-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /master/oss
 ls -al
-mkdir -p /master/oss/dist
-chmod a+w /master/oss/dist
-ls -al
+#mkdir -p /master/oss/dist
+#chmod a+w /master/oss/dist
+#ls -al
 python3 generator/build.py --build 5.4.3
