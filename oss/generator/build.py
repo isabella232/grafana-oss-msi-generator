af511f2d2138c8d78a230cdba71fd97176d88404
Brian Gann <bkgann@gmail.com>
2019-02-19 21:31:34 -0600

    fix final msi rename

--- oss/generator/build.py
+++ oss/generator/build.py
@@ -222,7 +222,7 @@ def build_oss(zip_file, extracted_name, PRODUCT_VERSION, config, features):
     except Exception as ex:
         print(ex)
     # copy to scratch with version included
-    msi_filename = '/tmp/scratch/grafana-{}.windows-amd64.msi'.format(PRODUCT_VERSION)
+    msi_filename = '/tmp/scratch/{}.windows-amd64.msi'.format(extracted_name)
     shutil.copy2('grafana.msi', msi_filename)
     os.system('ls -al /tmp/scratch')
     print("LIGHT COMPLETE")
